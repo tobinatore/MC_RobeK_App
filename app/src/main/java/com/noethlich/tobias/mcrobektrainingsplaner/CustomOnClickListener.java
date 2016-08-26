a74377d973bab6d6c9655f7ee6a6fbd4f2509817
Tobias Nöthlich <germanicalp@gmail.com>
2016-08-26 18:46:04 +0200

    Commit #3 + added permission for using bluetooth + added check if phone has bluetooth capabilities + added possibility to activate bluetooth from app + added dialog for choosing whom to connect to

--- app/src/main/java/com/noethlich/tobias/mcrobektrainingsplaner/CustomOnClickListener.java
+++ app/src/main/java/com/noethlich/tobias/mcrobektrainingsplaner/CustomOnClickListener.java
@@ -1,19 +1,30 @@
 package com.noethlich.tobias.mcrobektrainingsplaner;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
+
 
 /**
  * Created by Tobias on 09.08.2016.
  */
 public class CustomOnClickListener implements View.OnClickListener {
 
+    private final int REQUEST_ENABLE_BT = 1;
     private final int position;
     Context context;
     DatabaseHandler db;
     Button button;
+    ArrayAdapter mArrayAdapter;
 
     public CustomOnClickListener(int position, Context cont, Button btn) {
         this.position = position;
@@ -22,9 +33,84 @@ public class CustomOnClickListener implements View.OnClickListener {
         this.button = btn;
     }
 
-    public void onClick(View v)
-    {
-        Toast.makeText(context, db.getTraining((Integer) button.getTag(R.id.training_id))._combo.toString(), Toast.LENGTH_SHORT).show();
+    public void onClick(View v) {
+        //Toast.makeText(context, db.getTraining((Integer) button.getTag(R.id.training_id))._combo.toString(), Toast.LENGTH_SHORT).show();
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(context, "Ihr Gerät unterstützt leider kein Bluetooth,", Toast.LENGTH_LONG);
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            Intent discoverableIntent = new
+                    Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            context.startActivity(discoverableIntent);
+
+
+        }
+        else if(mBluetoothAdapter.isEnabled()){
+            mArrayAdapter = new ArrayAdapter(context,android.R.layout.select_dialog_singlechoice);
+            mBluetoothAdapter.startDiscovery();
+            // Create a BroadcastReceiver for ACTION_FOUND
+             final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+                public void onReceive(Context context, Intent intent) {
+                    String action = intent.getAction();
+                    // When discovery finds a device
+                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                        // Get the BluetoothDevice object from the Intent
+                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                        // Add the name and address to an array adapter to show in a ListView
+                        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                        showDialog();
+                    }
+                }
+            };
+            // Register the BroadcastReceiver
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            context.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+        }
+    }
+
+    public void showDialog(){
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(context);
+        builderSingle.setTitle("Wähle bitte MC RobeK aus:");
+
+        builderSingle.setNegativeButton(
+                "cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mArrayAdapter.clear();
+                        dialog.dismiss();
+                    }
+                });
+
+        builderSingle.setAdapter(
+                mArrayAdapter,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = mArrayAdapter.getItem(which).toString();
+                        AlertDialog.Builder builderInner = new AlertDialog.Builder(
+                                context);
+                        builderInner.setMessage(strName);
+                        builderInner.setTitle("Your Selected Item is");
+                        builderInner.setPositiveButton(
+                                "Ok",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(
+                                            DialogInterface dialog,
+                                            int which) {
+                                        mArrayAdapter.clear();
+                                        dialog.dismiss();
+                                    }
+                                });
+                        builderInner.show();
+                    }
+                });
+        builderSingle.show();
     }
 }
 
